4c16931678326b6abc292c9964c9af5675548d2f
gomathisubbu <gomathi.mel@gmail.com>
2023-02-09 00:09:12 +0530

    New line added

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -61,6 +61,7 @@ public class BaseClass {
 	public static void toQuit() {
 		driver.quit();
 	System.out.println("Prajit Updated");
+	System.out.println("New line added by Prajit");
 	}
 	
 	public static void toPerformMoveElement(WebElement element) {
